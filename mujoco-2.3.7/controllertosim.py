4732b7cf19ae89c76a25a54f1b5543f83804f0e5
JasperTan97 <jastan@student.ethz.ch>
2023-09-13 08:03:45 +0000

    feat: seperate simulation and controller programs

---
+++ mujoco-2.3.7/controllertosim.py
@@ -0,0 +1,52 @@
+"""
+This program connects with simtocontroller via localhost.
+Writes control input for publishing
+Subscribes to state information.
+
+In the future, this will be connected to a controller
+"""
+import zmq, clproto
+from state_representation import JointState
+
+from network_interfaces.zmq import network
+
+context = zmq.Context(1)
+publisher = network.configure_publisher(context, '127.0.0.1:6001', False)  # state
+subscriber = network.configure_subscriber(context, '127.0.0.1:6000', False)  # command
+
+class Robot:
+    nu = 6
+    nq = 6
+    joint = ["shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint"]
+
+def receive_encoded_state(subscriber, wait=False):
+    zmq_flag = 0 if wait else zmq.DONTWAIT
+    try:
+        message = subscriber.recv(zmq_flag)
+    except zmq.error.Again:
+        return None
+
+    if message:
+        return message
+
+model = Robot()
+
+control_input = JointState().Random("robot", ['ur5e_' + model.joint[q] for q in range(model.nq)])
+
+i=0
+print_state_output = False # for debug, prints state of robot occasionally
+while True:
+    for u in range(model.nu):
+        control_input.set_velocity(0, u)
+        if u == 0:
+            control_input.set_velocity(-2, u)
+
+    publisher.send(clproto.encode(control_input, clproto.MessageType.JOINT_STATE_MESSAGE))
+    message = receive_encoded_state(subscriber)
+    if message:
+        state_output = clproto.decode(message)
+        if state_output and print_state_output:
+            i+=1
+            if i % 10000 == 0:
+                print(state_output)
+                i=0
--- mujoco-2.3.7/simtocontroller.py
+++ mujoco-2.3.7/simtocontroller.py
@@ -1,46 +1,60 @@
-import time
-import _thread
+"""
+This program connects with controllertosim via localhost.
+Publishes state information and subscribes control input to and from controllertosim respectively
+Launches MuJoCo viewer and visualises robot state
+"""
+from threading import Thread
 
 import mujoco
 from mujoco import viewer
 
+import zmq, clproto
 from state_representation import JointState
+
+from network_interfaces.zmq import network
+
+context = zmq.Context(1)
+publisher = network.configure_publisher(context, '*:6000', True)  
+subscriber = network.configure_subscriber(context, '*:6001', True) 
 
 model = mujoco.MjModel.from_xml_path('universal_robots_ur5e/scene.xml')
 data = mujoco.MjData(model)
 
 state_output = JointState().Random("robot", ['ur5e_' + model.joint(q).name for q in range(model.nq)])
-control_input = JointState().Random("robot", ['ur5e_' + model.joint(q).name for q in range(model.nq)])
 
-def mujoco_sim():
-    viewer.launch(model, data)
+def receive_encoded_state(subscriber, wait=False):
+    # tries to receive message via connection
+    zmq_flag = 0 if wait else zmq.DONTWAIT
+    try:
+        message = subscriber.recv(zmq_flag)
+    except zmq.error.Again:
+        return None
 
-def updator():
-    i=0
-    while True:
-        for u in range(model.nu):
-            control_input.set_velocity(0, u)
-            if u == 0:
-                control_input.set_velocity(-2, u)
+    if message:
+        return message
 
-        for u in range(model.nu):
-            data.ctrl[u] = control_input.get_velocity(u)
-
+def communication_loop(run):
+    # runs continuously, to write command input into mujoco
+    # and to pubsub from controllertosim
+    while run:
         for q in range(model.nq):
-                    state_output.set_position(data.qpos[q], q)
-                    state_output.set_velocity(data.qvel[q], q)
-        i+=1
+            state_output.set_position(data.qpos[q], q)
+            state_output.set_velocity(data.qvel[q], q)
 
-        if i % 10000 == 0:
-            print(state_output)
-            # print(data.ctrl)
-            i=0
+        publisher.send(clproto.encode(state_output, clproto.MessageType.JOINT_STATE_MESSAGE))
+        message = receive_encoded_state(subscriber)
+        if message:
+            command = clproto.decode(message)
+        
+            if command:
+                for u in range(model.nu):
+                    data.ctrl[u] = command.get_velocity(u)
 
-try:
-   _thread.start_new_thread( mujoco_sim, () )
-   _thread.start_new_thread( updator, () )
-except:
-   print("Error: unable to start thread")
+run_thread = True
+t = Thread(target=communication_loop, args=[run_thread])
+t.start()
 
-while 1:
-   pass
+viewer.launch(model, data)
+
+run_thread = False
+t.join()
